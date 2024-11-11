182cef805c35847a5b7db2fb0e4b270d92ff01f7
Jerry Jiang <jerryjiang@Jerrys-Mac-mini.local>
2024-11-11 17:15:41 +1300

    change nrow error

--- R/data_type.R
+++ R/data_type.R
@@ -1,7 +1,8 @@
 data_type <-
 function(row, question, data){
   sm_cols <- data$col_names
-  if (!(row %in% 1:nrow(test_A)) || !(question %in% sm_cols))
+  d <- data$data
+  if (!(row %in% 1:nrow(d)) || !(question %in% sm_cols))
     stop("Invalid input")
   sm <- data$sm
   column <- which(sm_cols == question)
